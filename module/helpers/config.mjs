6260157817a75ba57c60efba247791865b7fe60d
MrTheBino <mrthebino@gmail.com>
2025-03-27 09:13:15 +0100

    initial

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const BPNB_BORG = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+BPNB_BORG.abilities = {
+  str: 'BPNB_BORG.Ability.Str.long',
+  dex: 'BPNB_BORG.Ability.Dex.long',
+  con: 'BPNB_BORG.Ability.Con.long',
+  int: 'BPNB_BORG.Ability.Int.long',
+  wis: 'BPNB_BORG.Ability.Wis.long',
+  cha: 'BPNB_BORG.Ability.Cha.long',
+};
+
+BPNB_BORG.abilityAbbreviations = {
+  str: 'BPNB_BORG.Ability.Str.abbr',
+  dex: 'BPNB_BORG.Ability.Dex.abbr',
+  con: 'BPNB_BORG.Ability.Con.abbr',
+  int: 'BPNB_BORG.Ability.Int.abbr',
+  wis: 'BPNB_BORG.Ability.Wis.abbr',
+  cha: 'BPNB_BORG.Ability.Cha.abbr',
+};
